99f2f33199b2ad02aa1a1b5aa50ea8c3bfdbd852
Brayan Escouto <110268410+BraiyaGhst@users.noreply.github.com>
2023-06-01 11:58:32 -0300

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,78 +1,60 @@
-window.onload = function () {
+window.onload = function() {
+  let seconds = 0;
+  let tens = 0;
+  let minutes = 0;
+  let appendTens = document.getElementById("tens");
+  let appendSeconds = document.getElementById("seconds");
+  let appendMinutes = document.getElementById("minutes");
+  let buttonStart = document.getElementById("button-start");
+  let buttonStop = document.getElementById("button-stop");
+  let buttonReset = document.getElementById("button-reset");
+  let interval;
 
-    let seconds = 00 ;
-    let tens = 00;
-    let minutes = 00
-    let appendTens = 
-    document.getElementById("tens")
-    let appendSeconds =
-    document.getElementById("seconds")
-    let appendMinutes =
-   document.getElementById("minutes")
-   let buttonStart = 
-    document.getElementById('button-start')
-    let buttonStop =
-    document.getElementById('button-stop')
-    let buttonReset =
-    document.getElementById('button-reset')
-    let interval ;
-
-    buttonStart.onclick = function() {
-
-        clearInterval (interval);
-        interval = setInterval(startTimer, 10);
-    }
-
-    buttonStop.onclick = function () {
+  buttonStart.onclick = function() {
     clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+  };
+
+  buttonStop.onclick = function() {
+    clearInterval(interval);
+  };
+
+  buttonReset.onclick = function() {
+    clearInterval(interval);
+    tens = 0;
+    seconds = 0;
+    minutes = 0;
+    appendTens.innerHTML = "00";
+    appendSeconds.innerHTML = "00";
+    appendMinutes.innerHTML = "00";
+  };
+
+  function startTimer() {
+    tens++;
+
+    if (tens <= 9) {
+      appendTens.innerHTML = "0" + tens;
     }
-    buttonReset.onclick = function() {
-     clearInterval (interval);
-    tens = "00"
-    seconds = "00"
-    minutes = "00"
-    appendTens.innerHTML = tens;
-    appendSeconds.innerHTML = seconds;
-    appendMinutes.innerHTML = minutes
+    if (tens > 9) {
+      appendTens.innerHTML = tens;
     }
-
-    function startTimer () {
-        tens++;
-
-        if(tens <= 9){
-            appendTens.innerHTML= "0" + tens;
+    if (tens > 99) {
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + tens;
     }
-        if(tens > 9) {
-            appendTens.innerHTML = tens;
-
-        }
-        if (tens > 99) {
-            console.log("seconds");
-            seconds++;
-            appendSeconds.innerHTML = "0" + seconds;
-            tens = 0 
-            appendTens.innerHTML = "0" + 0
-        }
-        if (seconds <= 9 ) {
-            appendSeconds.innerHTML = "0" + seconds
-               
-        }
-        if (seconds > 9) {
-            appendSeconds.innerHTML = seconds
-        }        
-        if (seconds >= 60 ) {
-            console.log("minutes");
-            minutes++ 
-            appendMinutes.innerHTML = "0" + minutes;
-            seconds = 0;
-            appendSeconds.innerHTML = "0" + seconds;
-            }
-
-        if (minutes > 9){
-            appendMinutes.innerHTML = minutes
-        
-        }
-
+    if (seconds <= 9) {
+      appendSeconds.innerHTML = "0" + seconds;
     }
-
-}
+    if (seconds >= 60) {
+      minutes++;
+      appendMinutes.innerHTML = "0" + minutes;
+      seconds = 0;
+      appendSeconds.innerHTML = "0" + seconds;
+    }
+    if (minutes > 9) {
+      appendMinutes.innerHTML = minutes;
+    }
+  }
+};
